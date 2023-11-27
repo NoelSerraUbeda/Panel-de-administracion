158d9eaf5e23cd71461baea7b46aa085a642654b
NoelSerraUbeda <neweonj@gmail.com>
2023-11-27 15:23:05 +0100

    animacion modal

--- js/filter.js
+++ js/filter.js
@@ -1,10 +1,10 @@
 export default (() => {
-
     const filterIcon = document.querySelector('.filter-icon');
     const filterModal = document.querySelector('.topnav-filter-form');
 
-    filterIcon.addEventListener('click', function () {
-        filterModal.classList.add('active');
+    filterIcon.addEventListener('click', () => {
+        filterModal.classList.toggle('active');
     });
 })();
+
 
