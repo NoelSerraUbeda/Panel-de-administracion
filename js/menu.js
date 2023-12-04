5e40416b2ebac655897ca3ff16fff45ec2319267
NoelSerraUbeda <neweonj@gmail.com>
2023-12-04 13:40:46 +0100

    prueba rama

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,129 @@
-export default (() => {
+class Menu extends HTMLElement {
+    constructor() {
+        super()
+        this.shadow = this.attachShadow({ mode: 'open' })
+        this.title = this.getAttribute('title')
+    }
+    connectedCallback() {
+        this.render()
+    }
+    render() {
 
-    const menuIcon = document.querySelector('.menu-icon');
-    const topnavMenu = document.querySelector('.topnav-menu');
+        this.shadow.innerHTML =
+        /*html*/`
 
-    menuIcon?.addEventListener('click', function () {
-        topnavMenu.classList.toggle('active');
-        menuIcon.classList.toggle("active");
-    });
-})();
+            <style>
+                
+                header {
+                    position: relative;
+                    
+                }
+
+                .menu-icon {
+                    position: relative;
+                    background: none;
+                    cursor: pointer;
+                    color: inherit;
+                    display: block;
+                    font: inherit;
+                    border: none;
+                    padding: 5px;
+                }
+
+                .bar {
+                    background-color: white;
+                    transition: 0.8s;
+                    margin: 6px 0;
+                    height: 5px;
+                    width: 50px;
+                }
+
+                .menu-icon.active .bar:nth-child(1) {
+                    transform: rotate(-405deg) translate(-7px, 8px);
+                }
+
+                .menu-icon.active .bar:nth-child(2) {
+                    width: 0;
+                    background-color: hsl(240, 65%, 49%);
+                }
+
+                .menu-icon.active .bar:nth-child(3) {
+                    transform: rotate(405deg) translate(-7px, -8px);
+                }
+
+                .topnav-menu {
+                    background-image: linear-gradient(to bottom, hsl(0, 0%, 100%), hsl(39, 67%, 92%));
+                    border: 5px solid hsl(211, 100%, 50%);
+                    transition: height 0.5s, visibility 0.5s;
+                    border-radius: 25px;
+                    position: absolute;
+                    overflow: hidden;
+                    visibility: hidden;
+                    height: 0px;
+                    margin: 5px;
+                    width: 98%;
+                    left: 10px;
+                    top: 60px;
+                }
+
+                .topnav-menu-content {
+                    justify-content: center;
+                    align-items: start;
+                    padding: 10px;
+                    display: flex;
+                    gap: 100px;
+                }
+
+                .topnav-menu.active {
+                    height: 90vh;
+                    visibility: visible;
+                }
+
+
+                .topnav-menu-content h4 {
+                    color: hsl(0, 0%, 96%);
+                }
+
+                .topnav-menu-content-card {
+                    border: 5px solid hsl(211, 100%, 50%);
+                    background-color: hsl(195, 53%, 79%);
+                    border-radius: 25px;
+                    margin-top: 50px;
+                    padding: 10px;
+                    width: 33%;
+                }
+
+
+            </style>
+
+            <nav>
+                <button class="menu-icon">
+                    <div class="bar"></div>
+                    <div class="bar"></div>
+                    <div class="bar"></div>
+                </button>
+            </nav>
+            <div class="topnav-menu">
+                <div class="topnav-menu-content">
+                    <div class="topnav-menu-content-card">
+                        <h2>Contenido</h2>
+                    </div>
+
+                    <div class="topnav-menu-content-card">
+                        <h2>Contenido</h2>
+                    </div>
+                </div>
+            </div>
+        `
+        const menuIcon = this.shadow.querySelector('.menu-icon');
+        const topnavMenu = this.shadow.querySelector('.topnav-menu');
+    
+        menuIcon?.addEventListener('click', function () {
+            topnavMenu.classList.toggle('active');
+            menuIcon.classList.toggle("active");
+        });
+
+    }
+}
+
+customElements.define('menu-component', Menu);
